dab941ddd389ae5379826e2785f4b4b41253956a
Gilbert Young <109980169+bud-primordium@users.noreply.github.com>
2024-10-18 02:06:03 +0800

    Refactor file collection and update file names

--- collect.py
+++ collect.py
@@ -1,14 +1,14 @@
 import os
 
 # 输出文件名
-output_file = "output.txt"
+all_file = "all.txt"
 
 # 指定要遍历的文件夹路径
-folders = ["./src", "./tests"]
+all_folders = ["./src", "./tests"]
 
 # 打开文件以追加写入并设置编码为 UTF-8
-with open(output_file, "w", encoding="utf-8") as outfile:
-    for folder in folders:
+with open(all_file, "w", encoding="utf-8") as outfile:
+    for folder in all_folders:
         for root, _, files in os.walk(folder):
             for file in files:
                 # 只处理 .cpp 和 .py 文件
@@ -17,3 +17,16 @@ with open(output_file, "w", encoding="utf-8") as outfile:
                     with open(file_path, "r", encoding="utf-8") as infile:
                         outfile.write(infile.read())
                         outfile.write("\n\n")  # 每个文件间加入换行
+
+src_file = "src.txt"
+
+# 打开文件以追加写入并设置编码为 UTF-8
+with open(src_file, "w", encoding="utf-8") as outfile:
+    for root, _, files in os.walk("./src"):
+        for file in files:
+            # 只处理 .cpp 和 .py 文件
+            if file.endswith(".cpp") or file.endswith(".py"):
+                file_path = os.path.join(root, file)
+                with open(file_path, "r", encoding="utf-8") as infile:
+                    outfile.write(infile.read())
+                    outfile.write("\n\n")  # 每个文件间加入换行
