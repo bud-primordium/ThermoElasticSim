6210e0dc87d52c188a1c828e98dd3c7613ddb81f
Gilbert Young <109980169+bud-primordium@users.noreply.github.com>
2024-10-31 12:00:21 +0800

    2.4:可视化日志，能量守恒，网格搜索，增加变形数

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ThermoElasticSim",
-    version="2.3",
+    version="2.4",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
