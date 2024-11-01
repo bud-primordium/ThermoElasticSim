b201e19b01ed49a13e5f1ec737dc55d8292fd768
Gilbert Young <109980169+bud-primordium@users.noreply.github.com>
2024-11-01 14:03:22 +0800

    2.6 引入EAM，cell增加超胞

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ThermoElasticSim",
-    version="2.5",
+    version="2.6",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
