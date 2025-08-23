aecaa09dd258f31ffb25e88eccae4d71b4da7165
Gilbert <109980169+bud-primordium@users.noreply.github.com>
2025-08-23 14:55:37 +0800

    refactor: remove deprecated fortran directory

--- src/thermoelasticsim/fortran/test.f90
+++
@@ -1,9 +0,0 @@
-module test_module
-    use iso_c_binding
-    implicit none
-contains
-    subroutine test_sub()
-        integer(c_int) :: dummy_var
-        print *, "Testing C bindings."
-    end subroutine test_sub
-end module test_module
